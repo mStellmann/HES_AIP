9442c834853f5c5f7a235feb57e1fb19bf05369d
Matthias Stellmann <matthias.stellmann@haw-hamburg.de>
2013-04-22 22:00:28 +0200

    added logic stuff

--- src/komponentenInterfaces/intern/ITransportdienstleisterAdapter.java
+++ src/komponentenInterfaces/intern/ITransportdienstleisterAdapter.java
@@ -1,9 +1,13 @@
 package komponentenInterfaces.intern;
 
+import interfaces.ILieferung;
+import interfaces.ITransportauftrag;
+
 /**
  * User: Matthias
  * Date: 22.04.13
  * Time: 21:05
  */
 public interface ITransportdienstleisterAdapter {
+    ITransportauftrag erstelleTransportauftrag(int transportauftragsNummer, String transportdienstleister, ILieferung lieferung);
 }
