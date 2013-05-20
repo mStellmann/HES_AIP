91c0c0699bd049b03a5585645bb09d749a7859ac
Grzegorz Markiewicz <g.markiewicz@hotmail.de>
2013-05-20 20:03:23 +0200

    RMI Servererkennung

--- src/gui/ClientStarter.java
+++ src/gui/ClientStarter.java
@@ -1,12 +1,35 @@
 package gui;
 
+import serverFassaden.CallCenterServerFassade;
+
+import java.rmi.Naming;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.rmi.Naming.*;
+
 /**
  *
  */
 public class ClientStarter {
-    // TDOO
-    // Monitor starten
-    // Dispatcher starten
-    // ClientGUI starten
-    // DashboardGUI starten
+
+    public static void main(String[] args) throws Exception {
+
+        // TODO
+        // Monitor starten
+        // Dispatcher starten
+        // ClientGUI starten
+        // DashboardGUI starten
+
+        HashMap<String, CallCenterServerFassade> serverMap;
+        serverMap = new HashMap<String,CallCenterServerFassade>();
+
+        for(int i=0;i<args.length;i=i+2){
+        //args[0] = zB localhost     + name des Servers = args[1] ( entspricht args[0] bei SystemRunner Main)
+        String url = new String("rmi://" + args[i] + "/" + args[i+1]);
+        serverMap.put(args[i+1],(CallCenterServerFassade) lookup(url));
+        }
+    }
 }
