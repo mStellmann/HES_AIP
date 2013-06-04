b1c5fba21cefbf16f86aac80c3f66446c2fc6dd3
Maniac87 <roman.nastachenko@haw-hamburg.de>
2013-06-04 17:30:05 +0200

    alle bis auf die von mattis

--- src/tdl/TDLRunner.java
+++ src/tdl/TDLRunner.java
@@ -3,14 +3,17 @@ package tdl;
 import com.sun.net.httpserver.*;
 import com.sun.jersey.api.container.httpserver.HttpServerFactory;
 import javax.swing.*;
+import java.io.IOException;
 
 
 public class TDLRunner {
-    // TODO - main() - rcv von Jersey
-    // https://jersey.java.net/nonav/documentation/latest/client.html
-
            public static void main(String args[]) {
-               HttpServer server = HttpServerFactory.create( "http://localhost:8080/rest" );
+               HttpServer server = null;
+               try {
+                   server = HttpServerFactory.create("http://localhost:8080/rest");
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
                server.start();
                JOptionPane.showMessageDialog( null, "Ende" );
                server.stop( 0 );
