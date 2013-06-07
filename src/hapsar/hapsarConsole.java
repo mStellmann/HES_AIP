5709c4b93c04517208f3c46bcafb4857ab5e3451
mStellmann <matthias.stellmann@haw-hamburg.de>
2013-06-07 10:59:34 +0200

    sqdiagram added for haspar connection with rabbitMQ

--- src/hapsar/hapsarConsole.java
+++ src/hapsar/hapsarConsole.java
@@ -12,13 +12,9 @@ import java.util.Scanner;
  *
  */
 public class HAPSARConsole {
-    // TODO - main() + console -> writeToQueue() [publish]
-    // https://github.com/rabbitmq/rabbitmq-tutorials/tree/master/java
-
     private final static String QUEUE_NAME = "Zahlungseingang";
 
     public static void main(String[] argv) throws Exception {
-
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
@@ -28,7 +24,6 @@ public class HAPSARConsole {
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         Scanner sc = new Scanner(System.in);
 
-
         String message = null;
         System.out.println("Gregstar, Mattis und Roman Co. Trademark etc. PP usw.\n");
         System.out.println("Willkommen im Verwaltungssystem der HAPSAR!");
@@ -37,21 +32,18 @@ public class HAPSARConsole {
         System.out.println("Wenn sie lust haben, koennen sie dieses System durch das eintippen von >exit< beenden ");
         System.out.println("oder auch nicht - uns egal GG ");
 
-        while(!message.equals("exit")){
-        System.out.println("Bitte geben sie die Rechnungsnummer und den Betrag durch ein Leerzeichen getrennt ein:");
+        while (!message.equals("exit")) {
+            System.out.println("Bitte geben sie die Rechnungsnummer und den Betrag durch ein Leerzeichen getrennt ein:");
 
-        message = sc.nextLine();
+            message = sc.nextLine();
 
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-        System.out.println(" [x] Sent '" + message + "'");
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
         }
-
 
         channel.close();
         connection.close();
-
     }
 }
